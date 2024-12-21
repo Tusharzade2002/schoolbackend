a3bf0b7dd727b2e4c705d06f931beae8348e7b09
Tusharzade <tusharzade29@gmail.com>
2024-12-21 11:19:30 +0530

    dotenv installed

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import cors from 'cors'
+import cors from 'cors';
+import dotenv from 'dotenv'
  const app=express();
- 
+ dotenv.config();
 app.use(express.json());
 app.use(cors())
 
@@ -203,7 +204,7 @@ app.get("*",(req,res)=>{
         message:"invalid"
     })
 })
-const PORT=5005;
+const PORT=process.env.PORT;
 app.listen(PORT,()=>{
     console.log(`server is running on ${PORT}`);
     
